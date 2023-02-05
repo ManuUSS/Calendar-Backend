421efccb5ce11cb9b94ef34f7300ca96171ee6a2
Manuel Ulate <fr3zs09@gmail.com>
2023-02-05 14:36:07 -0600

    variables de entorno

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
+require('dotenv').config();
 
 const app = express();
 
-app.get('/', ( req, res ) => {
-    console.log('Se requiere un slash')
-    res.json({ ok: true, message: 'Siuuu' });
-});
+app.use( express.static( 'public' ) );
 
-app.listen( 4000, () => {
-    console.log('Servidor corriendo en puerto 4000')
+// app.get('/', ( req, res ) => {
+//     console.log('Se requiere un slash')
+//     res.json({ ok: true, message: 'Siuuu' });
+// });
+
+app.listen( process.env.PORT, () => {
+    console.log(`Servidor corriendo en puerto ${ process.env.PORT }`)
 });
