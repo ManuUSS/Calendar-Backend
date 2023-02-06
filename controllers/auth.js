02fd126810b4a5de063463937f95ecc9e998a273
Manuel Ulate <fr3zs09@gmail.com>
2023-02-05 21:15:38 -0600

    custom middleware

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,14 +1,8 @@
 const { response } = require('express');
-const { validationResult } = require('express-validator');
 
 const createUser = ( req, res = response ) => {
 
     const { name, email, password } = req.body;
-
-    // return res.status( 400 ).json({ok: false, msg: 'El nombre debe ser mayor a 5 letras'});
-    const errors = validationResult( req );
-    if( !errors.isEmpty() ) return res.status( 400 ).json({ ok: false, errors: errors.mapped() });
-
 
     return res.status( 201 ).json({
         ok: true,
@@ -20,10 +14,8 @@ const createUser = ( req, res = response ) => {
 }
 
 const loginUser = ( req, res = response ) => {
-    const { email, password } = req.body;
 
-    const errors = validationResult( req );
-    if( !errors.isEmpty() ) return res.status( 400 ).json({ ok: false, errors: errors.mapped() });
+    const { email, password } = req.body;
 
     res.status( 200 ).json({
         ok: true,
--- routes/auth.js
+++ routes/auth.js
@@ -6,6 +6,7 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 const router = Router();
 const { createUser, renewUser, loginUser } = require('../controllers/auth');
+const { validateFields } = require('../middlewares/validateFields');
 
 router.post(
     '/new', 
@@ -13,6 +14,7 @@ router.post(
         check('name',  'El nombre es obligatorio').not().isEmpty(), 
         check('email',  'El email es obligatorio').isEmail(), 
         check('password','La contraseña debe de ser de 6 caracteres').isLength({ min: 6 }), 
+        validateFields
     ],
     createUser );
 
@@ -20,7 +22,8 @@ router.post(
     '/', 
     [
         check('email',  'El email es obligatorio').isEmail(), 
-        check('password','La contraseña debe de ser de 6 caracteres').isLength({ min: 6 }), 
+        check('password','La contraseña debe de ser de 6 caracteres').isLength({ min: 6 }),
+        validateFields 
     ], 
     loginUser );
 
