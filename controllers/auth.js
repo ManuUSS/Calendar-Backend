f14cdc7fe1205573e97371abd0ca4c246ba76cb1
Manuel Ulate <fr3zs09@gmail.com>
2023-02-05 20:59:09 -0600

    recuperar informacion del posteo

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,18 +2,31 @@ const { response } = require('express');
 
 
 const createUser = ( req, res = response ) => {
-    res.json({
-        ok: true
+
+    const { name, email, password } = req.body;
+
+    // return res.status( 400 ).json({ok: false, msg: 'El nombre debe ser mayor a 5 letras'});
+
+    return res.status( 201 ).json({
+        ok: true,
+        msg: 'registro',
+        name, 
+        email,
+        password
     })
 }
 
-const loginUser = ( req, res ) => {
+const loginUser = ( req, res = response ) => {
+    const { email, password } = req.body;
+
     res.json({
-        ok: true
+        ok: true,
+        email,
+        password
     })
 }
 
-const renewUser = ( req, res ) => {
+const renewUser = ( req, res = response ) => {
     res.json({
         ok: true
     })
