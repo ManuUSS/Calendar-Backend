c5bf647999720771f9116fe998915c5be0e3d038
Manuel Ulate <fr3zs09@gmail.com>
2023-02-06 21:34:22 -0600

    configuracion de cors

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const { dbConnection } = require('./database/config');
 require('dotenv').config();
 
 const app = express();
 
 dbConnection();
+
+app.use().cors();
 
 app.use( express.static( 'public' ) );
 
