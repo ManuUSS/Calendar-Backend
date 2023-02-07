b3bf0183899e67c951292458f2cccbdc38693f28
Manuel Ulate <fr3zs09@gmail.com>
2023-02-06 21:59:02 -0600

    validar datos necesarios del evento

--- routes/events.js
+++ routes/events.js
@@ -1,6 +1,9 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 const { validateJWT } = require('../middlewares/validateJWT');
+const { validateFields } = require('../middlewares/validateFields');
 const { getEvent, createEvent, updateEvent, deleteEvent } = require('../controllers/events');
+const { isDate } = require('../helpers/isDate');
 
 const router = Router();
 
@@ -8,8 +11,20 @@ const router = Router();
 router.use( validateJWT );
 
 router.get('/', getEvent);
-router.post('/', createEvent);
+
+router.post(
+    '/',
+    [
+        check('title','Titulo es obligatorio').not().isEmpty(),
+        check('start','Fecha de inicio es obligatorio').custom( isDate ),
+        check('end','Fecha de finalización es obligatorio').custom( isDate ),
+        validateFields
+    ], 
+    createEvent
+);
+
 router.put('/:id', updateEvent);
+
 router.delete('/:id', deleteEvent);
 
 module.exports = router;
